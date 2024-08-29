6e5952e5fb1417e3a5da71563bc3d1aef00e10e1
Eric Turcotte <eric.turcotte@codeboxx.biz>
2024-08-29 11:13:55 -0400

    adding test

--- cypress/e2e/quebec-dev/testing.cy.js
+++ cypress/e2e/quebec-dev/testing.cy.js
@@ -2,7 +2,22 @@ describe("Quebec-Dev e2e", () => {
   it("Visits Quebec-Dev web site", () => {
     cy.visit("https://sleepy-springs-78051-2160681ab30d.herokuapp.com/");
   });
-//   it.skip("Validate nav-bar elements TC-QD-001 ", () => {
-//     cy.get('.ml-10 > [href="/developers"]').should("be.visible");
-//   });
+  it("Validate nav-bar elements - TC-QD-001 -", () => {
+    cy.visit("https://sleepy-springs-78051-2160681ab30d.herokuapp.com/");
+    cy.get(".max-w-7xl > .justify-between > :nth-child(1)").should(
+      "be.visible"
+    );
+    cy.get('.ml-10 > [href="/developers"]')
+      .should("be.visible")
+      .and("have.text", "Developers");
+    cy.get('.ml-10 > [href="/pricing"]').and("have.text", "Pricing");
+    cy.get('.ml-10 > [href="/bootcamps"]').and("have.text", "Bootcamps");
+    cy.get('.ml-10 > [href="/affiliates"]').and(
+      "have.text",
+      "Affiliate program"
+    );
+    cy.get(".ml-4 > .text-red-100").and("have.text", "Sign in");
+    cy.get(".ml-4 > .relative").and("have.text", "Register");
+    cy.contains("Register");
+  });
 });
